e6a9d71e8a053fd9a8c54ab709fc41f00d61070e
kwiecien <k.d.kwiecinski@gmail.com>
2019-04-14 20:54:01 +0200

    basic: fix code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createStore} from 'redux';
 import {render} from 'react-dom'
-import todoApp from 'reducers/reducers';
+import todoApp from './reducers/reducers';
 import {Provider} from "react-redux";
 import App from "./components/App";
 

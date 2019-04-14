f0d5c06f7ff4f8262ed5167172c2aa95fca73ce0
kwiecien <k.d.kwiecinski@gmail.com>
2019-04-14 20:51:57 +0200

    basic: pass the store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+import React from 'react';
 import {createStore} from 'redux';
+import {render} from 'react-dom'
 import todoApp from 'reducers/reducers';
+import {Provider} from "react-redux";
+import App from "./components/App";
 
 const store = createStore(todoApp, window.STATE_FROM_SERVER);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);

8e47c1f6907c909f363f815dea0d5f380a8fa22d
kwiecien <k.d.kwiecinski@gmail.com>
2019-05-05 22:32:55 +0200

    advanced: navigate with React Router

--- todos-example/src/index.js
+++ todos-example/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
 import rootReducer from './reducers'
+import Root from "./components/Root";
 
 const store = createStore(rootReducer)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Root store={store}/>,
+    document.getElementById('root')
 )
